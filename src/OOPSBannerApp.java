890aa14a62a02cef4ebcc41d38dd3c2b8c1acad9
Krishanth R J <krishanth.raya@gmail.com>
2026-02-23 23:13:26 +0530

    Added UC1: Print OOPS to console

---
+++ src/OOPSBannerApp.java
@@ -0,0 +1,24 @@
+/**
+ * OOPSBannerApp
+ *
+ * UC1: Print the literal text "OOPS" to the console.
+ *
+ * This is the entry point of the application.
+ *
+ * @author kr3976
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Main method - entry point of Java application
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Print OOPS message
+        System.out.println("OOPS");
+
+    }
+
+}
